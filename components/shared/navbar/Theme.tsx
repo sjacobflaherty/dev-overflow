bc35c8f214d237dea010d61ccc049c236b4a118e
Jacob Flaherty <98563031+sjacobflaherty@users.noreply.github.com>
2023-10-05 07:38:20 -0700

    updates

---
+++ components/shared/navbar/Theme.tsx
@@ -0,0 +1,56 @@
+'use client';
+import React from 'react';
+import Image from 'next/image';
+
+import { useTheme } from '@/context/ThemeProvider';
+import { MenubarShortcut } from '@/components/ui/menubar';
+import {
+  Menubar,
+  MenubarMenu,
+  MenubarTrigger,
+  MenubarContent,
+  MenubarItem,
+  MenubarSeparator,
+} from '@radix-ui/react-menubar';
+
+const Theme = () => {
+  const { mode, setMode } = useTheme();
+
+  return (
+    <Menubar className="relative border-none bg-transparent shadow-none">
+      <MenubarMenu>
+        <MenubarTrigger>
+          {mode === 'light' ? (
+            <Image
+              src="/assets/icons/sun.svg"
+              width={20}
+              height={20}
+              alt="sun"
+              className="active-theme"
+            />
+          ) : (
+            <Image
+              src="/assets/icons/moon.svg"
+              width={20}
+              height={20}
+              alt="moon"
+              className="active-theme"
+            />
+          )}
+        </MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem>
+            New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+          </MenubarItem>
+          <MenubarItem>New Window</MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem>Share</MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem>Print</MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>
+  );
+};
+
+export default Theme;
