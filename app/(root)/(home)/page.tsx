9fed4ca6cd3af576572298143078894f3b20688f
Jacob Flaherty <98563031+sjacobflaherty@users.noreply.github.com>
2023-11-01 13:51:40 -0700

    updates

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,8 +1,12 @@
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div>
-     
-    </div>
-  )
+    <>
+      <div>
+        <h1>All Questions</h1>
+        <Link href="/ask-question"></Link>
+      </div>
+    </>
+  );
 }
